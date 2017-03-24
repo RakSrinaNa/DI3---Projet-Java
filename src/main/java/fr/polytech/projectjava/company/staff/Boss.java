cd5ba10fcb028000751fc3ba2c8686455136405c
Thomas Couchoud <zerderr@gmail.com>
2017-03-24 09:07:11 +0100

    Add doc

--- src/main/java/fr/polytech/projectjava/company/staff/Boss.java
+++ src/main/java/fr/polytech/projectjava/company/staff/Boss.java
@@ -1,6 +1,9 @@
 package fr.polytech.projectjava.company.staff;
 
 /**
+ * Represent the boss of a company.
+ * He's not an employee and does not need to check in/out.
+ *
  * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 23/03/2017.
  *
  * @author Thomas Couchoud
@@ -8,6 +11,12 @@ package fr.polytech.projectjava.company.staff;
  */
 public class Boss extends Person
 {
+	/**
+	 * Construct the boss with its name.
+	 *
+	 * @param lastName His/her last name.
+	 * @param firstName His/her first name.
+	 */
 	public Boss(String lastName, String firstName)
 	{
 		super(lastName, firstName);
